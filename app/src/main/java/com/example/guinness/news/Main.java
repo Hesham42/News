739c9f1a4ed841649e94512f63a8f4c837e93d15
hmostafa3bde <hmostafa3bde@outlook.com>
2017-09-22 02:56:47 +0200

    Initial commit

---
+++ app/src/main/java/com/example/guinness/news/Main.java
@@ -0,0 +1,81 @@
+package com.example.guinness.news;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+
+public class Main extends AppCompatActivity implements TabLayout.OnTabSelectedListener{
+    //This is our tablayout
+    private TabLayout tabLayout;
+
+    //This is our viewPager
+    private ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Adding toolbar to the activity
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+        //Initializing the tablayout
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+
+        //Adding the tabs using addTab() method
+        tabLayout.addTab(tabLayout.newTab().setText("Top Store"));
+        tabLayout.addTab(tabLayout.newTab().setText("FRENCH ELECTION 2017"));
+        tabLayout.addTab(tabLayout.newTab().setText("MOST READ"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        //Initializing viewPager
+        viewPager = (ViewPager) findViewById(R.id.pager);
+
+        //Creating our pager adapter
+        Pager adapter = new Pager(getSupportFragmentManager(), tabLayout.getTabCount());
+
+        //Adding adapter to pager
+        viewPager.setAdapter(adapter);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+
+
+        //Adding onTabSelectedListener to swipe views
+        tabLayout.setOnTabSelectedListener(this);
+    }
+
+    @Override
+    public void onTabSelected(TabLayout.Tab tab) {
+        viewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(TabLayout.Tab tab) {
+
+    }
+
+    @Override
+    public void onTabReselected(TabLayout.Tab tab) {
+
+    }
+}
---
+++ app/src/main/java/com/example/guinness/news/Pager.java
@@ -0,0 +1,45 @@
+package com.example.guinness.news;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by guinness on 11/04/17.
+ */
+
+public class Pager extends FragmentStatePagerAdapter {
+
+    //integer to count number of tabs
+    int tabCount;
+
+    //Constructor to the class
+    public Pager(FragmentManager fm, int tabCount) {
+        super(fm);
+        //Initializing tab count
+        this.tabCount= tabCount;
+    }
+
+    //Overriding method getItem
+    @Override
+    public Fragment getItem(int position) {
+        //Returning the current tabs
+        switch (position) {
+            case 0:
+             return   new tab1();
+            case 1:
+
+                return new tab2();
+            case 2:
+                return new tab3();
+            default:
+                return null;
+        }
+    }
+
+    //Overriden method getCount to get the number of tabs
+    @Override
+    public int getCount() {
+        return tabCount;
+    }
+}
